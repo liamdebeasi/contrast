0fe11d0ad14ad08e43d90adffc67ce951ed7ebf4
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-12 18:38:56 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/contrast/',
   plugins: [
     vue({
       template: {
