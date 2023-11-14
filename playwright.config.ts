de6a240e9685b77c059a753cd90c80604e6f4d04
Liam DeBeasi <2721089+liamdebeasi@users.noreply.github.com>
2023-11-13 22:10:30 -0500

    add screenshot test

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -33,39 +33,17 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
     },
-
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices['Pixel 5'] },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 13'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,9 +2,15 @@ import { test, expect } from '@playwright/test';
 import { AxeBuilder } from '@axe-core/playwright';
 
 test('should not have axe errors', async ({ page }) => {
-  await page.goto('http://127.0.0.1:3000');
+  await page.goto('/');
   
   const results = await new AxeBuilder({ page }).analyze();
   
   expect(results.violations).toStrictEqual([]); 
 });
+
+test('should not have visual regressions', async ({ page }) => {
+  await page.goto('/');
+  
+  await expect(page).toHaveScreenshot('contrast-default.png');
+})
