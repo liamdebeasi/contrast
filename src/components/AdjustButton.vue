2d6f70e4ac0b6858d106546d877c7ba0c969d380
Liam DeBeasi <2721089+liamdebeasi@users.noreply.github.com>
2024-04-11 21:32:10 -0400

    add success indicator

--- src/components/AdjustButton.vue
+++ src/components/AdjustButton.vue
@@ -1,17 +1,51 @@
 <script setup lang="ts">  
-  import { colorWand } from 'ionicons/icons';
+  import { colorWand, checkmark } from 'ionicons/icons';
+  import { computeContrast } from '../utils';
+  import { ref } from 'vue';
+
   const props = defineProps(['modelValue', 'foreground', 'hint']);
   const emit = defineEmits(['update:modelValue']);
+  
+  const showAdjustButton = ref(true);
 
   const adjustColor = () => {
-    emit('update:modelValue', '#0088cc');
+    const contrast = computeContrast(props.foreground, props.modelValue);
+    
+    if (contrast < 4.5) {
+      emit('update:modelValue', '#0088cc');
+    }
+    
+    showAdjustButton.value = false;
+    
+    // Show a visual indicator that the operation succeeded to the clipboard.
+    setTimeout(() => {
+      showAdjustButton.value = true;
+    }, 3000);
   }  
 </script>
 
 <template>
   <div class="button-wrapper">
-    <button @click="adjustColor()" class="app-icon" :title="props.hint" :aria-label="props.hint">
+    <button v-if="showAdjustButton" @click="adjustColor()" class="app-icon" :title="props.hint" :aria-label="props.hint">
       <ion-icon aria-hidden="true" :icon="colorWand"></ion-icon>
     </button>
+    <div class="app-icon" v-if="!showAdjustButton">
+      <ion-icon aria-label="Background color adjusted" :icon="checkmark" class="checkmark"></ion-icon>
+    </div>
+    <div class="status" role="status" aria-live="assertive">
+      <p v-if="!showAdjustButton">Background color adjusted</p>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  .checkmark {
+    cursor: default;
+  }
+  
+  .status {
+    opacity: 0;
+    position: absolute;
+    pointer-events: none;
+  }
+</style>
